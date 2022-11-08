4198c5e9846b8269db0072c5218d650b930ebc91
Emre Arapcic Uevak <EmreArapcicUevak@gmail.com>
2022-11-08 21:34:37 +0100

    Add a template structure for list nodes

--- Utilities/List.h
+++ Utilities/List.h
@@ -4,6 +4,10 @@
 
 #ifndef List_H_Included
 #define List_H_Included
-
 typedef unsigned int sizeT;
+
+template <typename VT> struct listNode {
+  listNode<VT> *Next = NULL, *Previous = NULL;
+  VT value;
+};
 #endif
