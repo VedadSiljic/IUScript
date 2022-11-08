880e55a154f64a6a569475262706e71cc14891ed
Emre Arapcic Uevak <EmreArapcicUevak@gmail.com>
2022-11-08 21:41:38 +0100

    Make a function for adding values into the list

--- Utilities/List.h
+++ Utilities/List.h
@@ -16,7 +16,37 @@ template <typename VT> class List {
         listNode<VT> *start = NULL, *end = NULL;
         sizeT size;
     public:
-        List(){}
-        ~List(){}
+        void Add(VT valueToAdd, const sizeT index) {
+            if (index > this->size)
+                throw "Invalid Position";
+            else if (index == size) {
+                if (!size)
+                    start = end = new listNode<VT>;
+                else {
+                    end->Next = new listNode<VT>;
+                    end->Next->Previous = end;
+                    end = end->Next;
+                }
+                end->value = valueToAdd;
+            } else {
+                listNode<VT> *temp = getInstance(index), *valueNodeToAdd = new listNode<VT>;
+                if (temp == start)
+                    start = valueNodeToAdd;
+
+                valueNodeToAdd->value = valueToAdd;
+                valueNodeToAdd->Previous = temp->Previous;
+                valueNodeToAdd->Next = temp;
+
+                if (valueNodeToAdd->Previous)
+                    valueNodeToAdd->Previous->Next = valueNodeToAdd;
+                temp->Previous = valueNodeToAdd;
+            }
+
+            this->size = this->size + 1;
+        } // Adds a value to the specific index
+        
+        void Add(VT valueToAdd) { Add(valueToAdd, this->size); }
+            List(){}
+            ~List(){}
 };
 #endif
