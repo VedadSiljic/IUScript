0312088fa45966a102b9734c4eef8efaae2b5174
Vedad <siljicvedad13@gmail.com>
2022-11-09 17:45:27 +0100

    Add skeleton

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
 
-int main(){
-    std::cout << "test";
+std::string read(std::string input) {
+    return input;
+}
+
+std::string eval(std::string input) {
+    return input;
+}
+
+std::string print (std::string input) {
+    return input;
+}
+
+std::string rep(std::string input) {
+    std::string ast = read(input);
+    std::string result = eval(ast);
+    return print(result);
+}
+
+int main() {
+
+    std::string input;
+    while(true) {
+        std::cout << "<user> ";
+        if (!std::getline(std::cin, input))
+            break;
+        std::cout << rep(input) << std::endl;
+    }
+
     return 0;
 }
